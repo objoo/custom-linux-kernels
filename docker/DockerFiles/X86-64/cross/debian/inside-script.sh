36b423bcb1b194bcaec7de9c1a91aa35aa450b39
Yann Dupont <yann@objoo.org>
2015-08-27 00:47:19 +0200

    somewhat working

--- docker/DockerFiles/X86-64/cross/debian/inside-script.sh
+++ docker/DockerFiles/X86-64/cross/debian/inside-script.sh
@@ -1,7 +1,11 @@
 cd /GIT
 git clone git://git.denx.de/u-boot.git
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-cd u-boot
+cd /GIT/u-boot
 make Bananapi_defconfig
 ARCH=arm CROSS_COMPILE=arm-none-eabi- make
 cp u-boot-sunxi-with-spl.bin /RESULT
+cd /GIT/linux-stable
+curl -L --retry 20 --retry-delay 2 https://raw.githubusercontent.com/objoo/custom-linux-kernels/master/config/ARM/bananapi/config-4.1-YDoSHM -o .config
+make -j3  ARCH=arm CROSS_COMPILE=arm-none-eabi- LOADADDR=0x40008000 uImage modules dtbs
+ARCH=arm CROSS_COMPILE=arm-none-eabi- INSTALL_MOD_STRIP=1 make modules_install
