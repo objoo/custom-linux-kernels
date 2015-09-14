0443af7db5b04dc765d44b4bd114d7aca32adb81
Yann Dupont <yann@objoo.org>
2015-09-14 21:31:53 +0200

    scripts for latest lts & latest mainline

--- docker/DockerFiles/X86-64/cross/debian/INSIDE/inside-script-latest-mainline.sh
+++ docker/DockerFiles/X86-64/cross/debian/INSIDE/inside-script-latest-mainline.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 cd /GIT
-git clone git://git.denx.de/u-boot.git
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+if [ -d u-boot ] ; then
+	echo "git already there"
+else
+	git clone git://git.denx.de/u-boot.git
+fi
+
+if [ -d linux-stable ] ; then
+	echo "git already there" 
+else
+	git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+fi
 
 cd /GIT/u-boot
 git pull
 
 make Bananapi_defconfig
-ARCH=arm CROSS_COMPILE=arm-none-eabi- make
+ARCH=arm CROSS_COMPILE="ccache arm-none-eabi-" make
 
 rm -rf /RESULT/*
 
@@ -16,10 +25,10 @@ cp u-boot-sunxi-with-spl.bin /RESULT
 cd /GIT/linux-stable
 git checkout master
 git pull
-git checkout v4.1.6
+git checkout linux-4.2.y
 
-curl -L --retry 20 --retry-delay 2 https://raw.githubusercontent.com/objoo/custom-linux-kernels/master/config/ARM/bananapi/config-4.1-YDoSHM -o .config
-make -j3  ARCH=arm CROSS_COMPILE=arm-none-eabi- LOADADDR=0x40008000 uImage modules dtbs
+curl -L --retry 20 --retry-delay 2 https://raw.githubusercontent.com/objoo/custom-linux-kernels/master/config/ARM/bananapi/config-4.2.y -o .config
+make -j3  ARCH=arm CROSS_COMPILE="ccache arm-none-eabi-" LOADADDR=0x40008000 uImage modules dtbs
 
 ARCH=arm CROSS_COMPILE=arm-none-eabi- INSTALL_MOD_STRIP=1 make modules_install
 tar cvfz /RESULT/modules.tar.gz /lib/modules/*
