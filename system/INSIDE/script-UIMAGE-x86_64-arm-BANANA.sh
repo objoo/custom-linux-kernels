65ba2eb04aa5638fa622fb3633b5c546a1358ff7
Yann Dupont <Yann.Dupont@univ-nantes.fr>
2017-02-25 00:43:56 +0100

    4.10 build for arm. + correct numer of cores used for arm compilation.

--- system/INSIDE/script-UIMAGE-x86_64-arm-BANANA.sh
+++ system/INSIDE/script-UIMAGE-x86_64-arm-BANANA.sh
@@ -2,7 +2,7 @@
 NBCPU=`cat /proc/cpuinfo  | grep processor | tail -1 | awk '{print $3}'`
 echo "$NBCPU processors used for compilation."
 
-make -j3  ARCH=arm CROSS_COMPILE=arm-none-eabi- LOADADDR=0x40008000 uImage modules dtbs
+make -j$NBCPU  ARCH=arm CROSS_COMPILE=arm-none-eabi- LOADADDR=0x40008000 uImage modules dtbs
 
 ARCH=arm CROSS_COMPILE=arm-none-eabi- INSTALL_MOD_STRIP=1 make modules_install
 
