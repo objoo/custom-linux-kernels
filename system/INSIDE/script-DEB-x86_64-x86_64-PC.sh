28465a73900f959be13270f8cf0ea7538db94483
Yann Dupont <Yann.Dupont@univ-nantes.fr>
2017-07-03 16:17:19 +0200

    now use stretch.

--- system/INSIDE/script-DEB-x86_64-x86_64-PC.sh
+++ system/INSIDE/script-DEB-x86_64-x86_64-PC.sh
@@ -2,4 +2,6 @@
 NBCPU=`cat /proc/cpuinfo  | grep processor | tail -1 | awk '{print $3}'`
 echo "$NBCPU processors used for compilation."
 
-/usr/bin/make-kpkg -j$NBCPU --initrd kernel_image kernel_headers
+#/usr/bin/make-kpkg -j$NBCPU --initrd kernel_image kernel_headers
+scripts/config --disable DEBUG_INFO
+make deb-pkg -j$NBCPU
